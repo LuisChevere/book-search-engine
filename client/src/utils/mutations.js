d5323a8ed3f476dcacd48be5f9cea6d68b4e1748
Luis <hairchevere@gmail.com>
2023-03-20 12:00:06 -0500

    created const for remove_book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -66,3 +66,20 @@ link
 }
 `;
 
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+removeBook(bookId: $bookId) {
+_id
+username
+email
+savedBooks {
+bookId
+authors
+description
+title
+image
+link
+}
+}
+}
+`;
