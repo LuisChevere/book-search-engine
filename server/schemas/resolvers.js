f1d897650f95b13b9577f8a10ae4bcbe3c562182
Luis <hairchevere@gmail.com>
2023-03-20 11:31:21 -0500

    created save book parameter

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,5 +36,16 @@ const resolvers = {
 
         return { token, user};
     },
+    saveBook: async (parent, { newBook}, context) => {
+        if(context.user) {
+            const updatedUser = await User.findByIdAndUpdate(
+                {_id: context.user._id},
+                {$push: {savedBooks: newBook}},
+                {new: true}
+            );
+            return updatedUser;
+        }
+        throw new AuthenticationError('You must be logged in!');
+    },
     
 }
