784b1cdb320c7cf2c3c070dff5ac24f8de7196aa
Luis <hairchevere@gmail.com>
2023-03-20 11:28:34 -0500

    created add user and log in mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -13,4 +13,28 @@ const resolvers = {
         },
     },
     
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, {email, password }) => {
+        const user = await User.findOne({email});
+
+        if (!user) {
+            throw new AuthenticationError('No user found!')
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+        }
+
+        const token = signToken(user);
+
+        return { token, user};
+    },
+    
 }
