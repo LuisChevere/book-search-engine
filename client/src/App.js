61a61d282a446b6fb216e0bd95c5f05ed91500cf
Luis <hairchevere@gmail.com>
2023-03-21 11:52:22 -0500

    updated Routes from switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider} from '@apollo/react-hooks';
-import ApolloClient from 'apollo-client';
+import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -25,11 +25,11 @@ function App() {
     <Router>
       <>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route exact path='/' Component={SearchBooks}/>
           <Route exact path='/saved' Component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong Page!</h1>} />
-          </Switch>
+          </Routes>
       </>
     </Router>
     </ApolloProvider>
